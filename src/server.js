25ca00bbfa1bd4fc0322d1df8d5c1beacb6ed287
lucasfemar <lucasdeveloper98l@gmail.com>
2024-03-23 10:19:53 -0300

    Serving index.html

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+    console.log('\x1b[36m%s\x1b[0m', 'Server running at http://localhost:8080');
+});
