0d2894b2351a92f77083b60ff1425e9d920ef5f4
lucasfemar <lucasdeveloper98l@gmail.com>
2024-03-23 12:12:17 -0300

    Fixing using bootstrap

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 import express from 'express';
 
 const app = express();
-
 app.use(express.static('public'));
-app.use(express.static('./node_modules/bootstrap/dist/'));
 
 app.listen(8080, () => {
     console.log('\x1b[36m%s\x1b[0m', 'Server running at http://localhost:8080');
