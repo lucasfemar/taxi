ce9b97238fe5434662c58ceaeaa969d859250012
lucasfemar <lucasdeveloper98l@gmail.com>
2024-03-25 19:23:54 -0300

    Fixing driver at the map and inital socket

--- public/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btnSubmit').addEventListener('click', (event) => {
-    event.preventDefault();
-    const iName = document.getElementById('iName');
-    const iCredential = document.getElementById('iCredential');
-    const iCarModel = document.getElementById('iCarModel');
-    const iCarPlate = document.getElementById('iCarPlate');
-});
--- src/server.js
+++ src/server.js
@@ -1,8 +1,58 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
 dotenv.config();
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const drivers = [];
+
+app.use(express.json());
 app.use(express.static('public'));
-app.listen(8080, () => {
+
+app.get('/', (request, response) => {
+    response.sendFile(join(__dirname, '../pages/index.html'));
+});
+
+app.post('/', (request, response) => {
+    const { name, credential, carModel, carPlate } = request.body;
+    console.log({ name, credential, carModel, carPlate });
+    const token = new Date();
+    try {
+        drivers.push({
+            name,
+            credential,
+            carModel,
+            carPlate,
+            token,
+        });
+        response.status(200).json({
+            token,
+            credential,
+        });
+    } catch (error) {
+        response.status(500).json({ error });
+    }
+});
+
+app.get('/map', (request, response) => {
+    response.sendFile(join(__dirname, '../pages/map.html'));
+});
+
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(8080, () => {
     console.log('\x1b[36m%s\x1b[0m', 'Server running at http://localhost:8080');
 });
