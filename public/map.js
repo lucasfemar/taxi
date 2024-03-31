45002836511edae0fa9e234eb133314dc15c72f2
lucasfemar <lucasdeveloper98l@gmail.com>
2024-03-31 01:59:15 -0300

    Add and Remove driver with socket

--- public/map.js
+++ public/map.js
@@ -1,7 +1,90 @@
-export default async function createMap() {
-    function addDriver(driver) {}
+export default function createMap() {
+    const state = {
+        drivers: {},
+    };
+    const observers = [];
+
+    function subscribe(observerFunction) {
+        observers.push(observerFunction);
+    }
+
+    function notifyAll(command) {
+        observers.forEach((observerFunction) => {
+            observerFunction(command);
+        });
+    }
+
+    function setState(newState) {
+        Object.assign(state, newState);
+    }
+
+    function addDriver(command) {
+        const id = command.id;
+        const name = command.name;
+        const credential = command.credential;
+        const carModel = command.carModel;
+        const carPlate = command.carPlate;
+        const position = command.position;
+
+        state.drivers[command.id] = {
+            id,
+            name,
+            credential,
+            carModel,
+            carPlate,
+            position,
+        };
+        console.log(`> Map: Driver added ${JSON.stringify(state.drivers[command.id])}`);
+
+        notifyAll({
+            type: 'new_driver',
+            id,
+            name,
+            credential,
+            carModel,
+            carPlate,
+            position,
+        });
+    }
+
+    function removeDriver(command) {
+        const id = command.id;
+
+        delete state.drivers[id];
+
+        notifyAll({
+            type: 'remove_driver',
+            id,
+        });
+    }
+
+    function updateDriverPosition(command) {
+        const id = command.id;
+        const name = command.name;
+        const credential = command.credential;
+        const carModel = command.carModel;
+        const carPlate = command.carPlate;
+        const position = command.position;
+        console.log(`> Map: Updating driver position ${JSON.stringify(driver)}`);
+
+        driver.position = position;
+        notifyAll({
+            type: 'update_driver',
+            id,
+            name,
+            credential,
+            carModel,
+            carPlate,
+            position,
+        });
+    }
 
     return {
+        state,
         addDriver,
+        removeDriver,
+        updateDriverPosition,
+        subscribe,
+        setState,
     };
 }
--- src/server.js
+++ src/server.js
@@ -1,47 +1,46 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import path, { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
-
+import createMap from '../public/map.js';
 dotenv.config();
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const drivers = [];
+const sockets = new Server(server);
 
 app.use(express.json());
 app.use(express.static('public'));
 
-app.post('/', (request, response) => {
-    const { name, credential, carModel, carPlate } = request.body;
-    console.log({ name, credential, carModel, carPlate });
-    const token = new Date();
-    try {
-        drivers.push({
-            name,
-            credential,
-            carModel,
-            carPlate,
-            token,
-        });
-        response.status(200).json({
-            token,
-            credential,
-        });
-    } catch (error) {
-        response.status(500).json({ error });
-    }
+const map = createMap();
+
+map.subscribe((command) => {
+    sockets.emit(command.type, command);
 });
 
-io.on('connection', (socket) => {
+sockets.on('connection', (socket) => {
+    const driverId = socket.id;
+
+    socket.emit('setup', map.state);
+
+    socket.on('add_driver', (driver) => {
+        driver.id = driverId;
+        console.log(`> Server: request new driver ${driver.id}`);
+        map.addDriver(driver);
+    });
+
+    socket.on('delete_driver', (driver) => {
+        console.log(`> Server: request remove driver ${driver.id}`);
+        map.removeDriver(driver);
+    });
+
+    socket.on('update_driver_position', (driverData) => {
+        console.log(`> Server: request new driver postition ${driverData.id}`);
+        map.updateDriverPosition(driverData);
+    });
+
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        map.removeDriver({ id: driverId });
     });
 });
 
