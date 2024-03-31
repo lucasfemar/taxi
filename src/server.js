ea6d275636d47d2ce10e54eecf6009c53d6d5b4d
lucasfemar <lucasdeveloper98l@gmail.com>
2024-03-31 12:59:26 -0300

    Exporting server

--- src/server.js
+++ src/server.js
@@ -49,3 +49,5 @@ app.get('/status', (request, reponse) => {
 server.listen(8080, () => {
     console.log('\x1b[36m%s\x1b[0m', 'Server running at http://localhost:8080');
 });
+
+module.exports = server;
